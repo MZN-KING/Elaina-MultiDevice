20296f51363492681072a0ede73b2224e6c7b31f
Mazeen Ahamed <106764948+MZN-KING@users.noreply.github.com>
2023-06-13 22:18:07 +0530

    Update youtube-yts.js

--- plugins/youtube-yts.js
+++ plugins/youtube-yts.js
@@ -3,7 +3,7 @@ import yts from 'yt-search'
 var handler = async(m, { conn, usedPrefix, text, args, command }) => {
 let name = await conn.getName(m.sender)
 
-  if (!text) throw 'Cari apa?'
+  if (!text) throw '*Please give me a text to search!*'
   let cari = await yts(`${text}`)
     let dapet = cari.all
     let listSections = []
