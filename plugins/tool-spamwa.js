5f685dd73bae03c129190294b10888bec6aa8952
Mazeen Ahamed <106764948+MZN-KING@users.noreply.github.com>
2023-06-13 23:39:20 +0530

    Update tool-spamwa.js

--- plugins/tool-spamwa.js
+++ plugins/tool-spamwa.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text }) => {
 let [nomor, pesan, jumlah] = text.split('|')
 if (!nomor) throw '*⚠️ Please Enter the value of number that will be spammed!*\n* Use Properly:*\n*—◉ #spamwa number|text|value*\n*Example:*\n*—◉ #spamwa 94999999999|Hello!|25*'
 if (!pesan) throw '*⚠️ Please message to put a message to spam!*\n*Use Properly:*\n*—◉ #spamwa number|text|value*\n*Example+*\n*—◉ #spamwa 94999999999|merespons :v|25*'
-if (jumlah && isNaN(jumlah)) throw '*[ ⚠️ ] KUANTITAS HARUS ADALAH NOMOR!*\n*PENGGUNAAN YANG TEPAT:*\n*—◉ #spamwa number|text|value*\n*Examplr:*\n*—◉ #spamwa 94999999999|merespons :v|25*'
+if (jumlah && isNaN(jumlah)) throw '*⚠️ Quantity Must be a number!*\n*Use Properly:*\n*—◉ #spamwa number|text|value*\n*Example:*\n*—◉ #spamwa 94999999999|merespons :v|25*'
 
 let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
 let fixedJumlah = jumlah ? jumlah * 1 : 10
-if (fixedJumlah > 50) throw '*[ ⚠️ ] TERLALU BANYAK PESAN! JUMLAH HARUS KURANG DARI 50 PESAN*️'
+if (fixedJumlah > 50) throw '*⚠️ Too many messages! Quantity must be less than 50 messages*️'
 await m.reply(`*[❗] SPAM PESAN KE NOMOR ${nomor} ITU SUKSES DILAKUKAN*\n*JUMLAH TERKIRIM:*\n*—◉ ${fixedJumlah} waktu!*`)
 for (let i = fixedJumlah; i > 1; i--) {
 if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m)
