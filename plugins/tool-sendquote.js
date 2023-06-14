8af34092448da94babb0797627b4e802457e6f32
Mazeen Ahamed <106764948+MZN-KING@users.noreply.github.com>
2023-06-14 17:29:05 +0530

    Update tool-sendquote.js

--- plugins/tool-sendquote.js
+++ plugins/tool-sendquote.js
@@ -1,7 +1,7 @@
 async function handler(m) {
-    if (!m.quoted) throw 'reply pesan!'
+    if (!m.quoted) throw '*Reply to a message!*'
     let q = await m.getQuotedObj()
-    if (!q.quoted) throw 'pesan yang anda reply tidak mengandung reply!'
+    if (!q.quoted) throw '*This message you reply to does not available!*'
     await q.quoted.copyNForward(m.chat, true)
 }
 handler.command = /^q$/i
